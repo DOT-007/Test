84d82f40a7b692009fff5fd16094cb4d6cd231ef
ALOSIOUS BENNY <149181456+DOT-007@users.noreply.github.com>
2024-11-08 21:09:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,38 +1,28 @@
 from flask import Flask, request, render_template, redirect, url_for
 import telebot
 import os
-from dotenv import load_dotenv
-
-# Load environment variables from .env file
-load_dotenv()
 
 # Initialize Flask app and Telebot
 app = Flask(__name__)
 TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 CHAT_ID = os.getenv("CHAT_ID")
 
-# Initialize the bot with the loaded token
 bot = telebot.TeleBot(TOKEN)
 
 @app.route('/')
 def index():
-    # Render the contact form
     return render_template('contact.html')
 
 @app.route('/submit', methods=['POST'])
 def submit():
-    # Retrieve data from the form
     name = request.form.get('name')
     email = request.form.get('email')
     message = request.form.get('message')
 
-    # Format message for Telegram
     telegram_message = f"New Contact Submission:\nName: {name}\nEmail: {email}\nMessage: {message}"
-
-    # Send message to Telegram
     bot.send_message(CHAT_ID, telegram_message)
 
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
